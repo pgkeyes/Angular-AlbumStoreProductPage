b99cec924c1c48c9c6306e4552d83dabf688283a
Peter <pgkeyes@hotmail.com>
2020-06-03 21:12:23 +0100

    Product list edits.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -19,7 +19,7 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map( resp => <Product[]>resp.json());
+    return this._http.get(this._productsUrl).map( response => <Product[]>response.json());
   }
 
 }
